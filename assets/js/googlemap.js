99c3914a11bdce9dedd88bc732f524f3f8c3d9bd
hansyao <hansyow@gmail.com>
2021-06-09 16:06:01 +0800

    make default showup in google map infowindow

--- assets/js/googlemap.js
+++ assets/js/googlemap.js
@@ -1,6 +1,6 @@
 function initMap() {
   var locations = [
-    ['Hans Yao', 31.132193301555073, 121.57718055197236, 'https://cdn.jsdelivr.net/gh/hansyao/image-hosting@master/20210520/image.40nrz6phtdg0.png']
+    ['<div><b>Hans Yao</b></div><div><b>email:&nbsp;&nbsp;</b><a href="mailto:hansyow@gmail.com">hansyow@gmail.com</a></div><div><b>电话:&nbsp;&nbsp;&nbsp;</b>+1(857)288-8870</div>', 31.132193301555073, 121.57718055197236, 'https://cdn.jsdelivr.net/gh/hansyao/image-hosting@master/20210520/image.40nrz6phtdg0.png']
   ];
 
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -9,7 +9,7 @@ function initMap() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
-  var infowindow = new google.maps.InfoWindow();
+
   var marker, i;
 
   for (i = 0; i < locations.length; i++) {
@@ -19,12 +19,18 @@ function initMap() {
       map: map
     });
 
+    var infowindow = new google.maps.InfoWindow({
+      content: locations[i][0]
+    });
+    infowindow.open(map, marker);
+
     google.maps.event.addListener(marker, 'click', (function (marker, i) {
       return function () {
         infowindow.setContent(locations[i][0]);
         infowindow.open(map, marker);
       }
     })(marker, i));
+    
   }
 
 }
